89b6ed64e277f92ac9ca6e819fddbfa5670fe6aa
RicardoVieraM <ricardoviera4m@gmail.com>
2025-02-24 14:44:38 +0100

    ensaladas db

---
+++ src/backend/obtener_productos.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Content-Type: application/json');
+include 'db.php'; // Incluir la conexión a la base de datos
+
+$sql = "SELECT * FROM productos";
+$result = $conn->query($sql);
+
+$productos = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $productos[] = $row;
+    }
+}
+
+echo json_encode($productos); // Enviar los datos como JSON
+$conn->close();
+?>
